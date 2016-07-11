03d5bc706a6a2eaf610bb28795b8f675bd390eae
Petar Spasic <spasicpetar@gmail.com>
2016-07-11 19:59:15 +0200

    6

---
+++ mysite/code/ArticleHolder.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Spale
+ * Date: 7/11/2016
+ * Time: 7:21 PM
+ */
+
+class ArticleHolder extends Page
+{
+    private static $allowed_children = ['ArticlePage'];
+}
+class ArticleHolder_Controller extends Page_Controller
+{
+
+}
---
+++ mysite/code/ArticlePage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Spale
+ * Date: 7/11/2016
+ * Time: 7:21 PM
+ */
+class ArticlePage extends Page
+{
+    private static $can_be_root = false;
+}
+class ArticlePage_Controller extends Page_Controller
+{
+
+}
