4db0d64fb687e259ea2b0e7498245387367b202b
Petar Spasic <spasicpetar@gmail.com>
2016-07-11 19:17:07 +0200

    5

---
+++ mysite/code/HomePage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Spale
+ * Date: 7/11/2016
+ * Time: 6:41 PM
+ */
+class HomePage extends Page
+{
+
+}
+class HomePage_Controller extends Page_Controller
+{
+
+}
