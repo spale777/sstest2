084f453559d4600d1488e418ca468d0d57f4e666
Petar Spasic <spasicpetar@gmail.com>
2016-07-12 20:15:50 +0200

    8

--- mysite/code/ArticlePage.php
+++ mysite/code/ArticlePage.php
@@ -13,6 +13,10 @@ class ArticlePage extends Page
         'Teaser' => 'Text',
         'Author' => 'Varchar',
     ];
+    private static $has_one = [
+        'Photo' => 'Image',
+        'Brochure' => 'File',
+    ];
 
     public function getCMSFields()
     {
@@ -22,6 +26,13 @@ class ArticlePage extends Page
             , 'Content');
         $fields->addFieldTotab('Root.Main', TextareaField::create('Teaser'), 'Content');
         $fields->addFieldToTab('Root.Main', TextField::create('Author', 'Author of article'), 'Content');
+        $fields->addFieldToTab('Root.Includes', $photo = UploadField::create('Photo','Article Photo'));
+        $fields->addFieldToTab('Root.Includes', $brochure = UploadField::create('Brochure', 'Brochure (PDF only)'));
+
+        $photo->getValidator()->setAllowedExtensions(['png', 'gif', 'jpg', 'jpeg']);
+        $photo->setFolderName('travel-photos');
+        $brochure->getValidator()->setAllowedExtensions(['pdf',]);
+        $brochure->setFolderName('travel-brochures');
 
         return $fields;
     }
