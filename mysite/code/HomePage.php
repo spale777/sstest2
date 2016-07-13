fe0d52747ee74783e888d22bcf5c35c7b8c95aba
Petar Spasic <spasicpetar@gmail.com>
2016-07-13 23:51:00 +0200

    9

--- mysite/code/HomePage.php
+++ mysite/code/HomePage.php
@@ -11,5 +11,10 @@ class HomePage extends Page
 }
 class HomePage_Controller extends Page_Controller
 {
-
+    public function LatestArticles($count = 3)
+    {
+        return ArticlePage::get()
+                    ->sort('Created','DESC')
+                    ->limit($count);
+    }
 }
